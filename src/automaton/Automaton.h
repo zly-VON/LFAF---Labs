4563c47e7803b50017b05b8655a421c98b90710a
zly-VON <113551094+zly-VON@users.noreply.github.com>
2023-03-06 23:55:16 +0200

    update lab2

--- src/automaton/Automaton.h
+++ src/automaton/Automaton.h
@@ -1,24 +1,41 @@
 #ifndef AUTOMATON_H
 #define AUTOMATON_H
 
-#include "Grammar.h"
 
-class FiniteAutomaton : public Grammar
+#include <iostream>
+#include <set>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+using namespace std;
+
+class Grammar;
+
+class Automaton
 {
     private:
-        vector<string> states;
-        vector<string> alphabet;
+        set<string> states;
+        set<string> alphabet;
         string initialState;
-        string finalState;
+        vector<string> finalStates;
 
         vector<string> currentState;
         vector<string> nextState;
         vector<string> transitionLabel;
     
     public:
-        FiniteAutomaton();
-        bool wordIsValid(string s);
+        Automaton(string initState, vector<string> finalSt);
+        void addTransitions(string currSt, string tranL, string nextSt);
+        void addFinalState(string finalSt);
+        void wordIsValid(string s);
         void printFiniteAutomaton();
+        Grammar toGrammar();
+        void isDeterministic();
+        void printGraph(string name);
+        bool findRecurrence(multimap<string,string> mp, string first, string second);
+        Automaton toDFA();
 };
+
 
 #endif
