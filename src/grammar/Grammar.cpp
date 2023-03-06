8968cb5927de86a267b9435753fe9f787eb620e3
zly-VON <113551094+zly-VON@users.noreply.github.com>
2023-03-06 23:54:33 +0200

    update lab2

--- src/grammar/Grammar.cpp
+++ src/grammar/Grammar.cpp
@@ -1,44 +1,41 @@
 #include "Grammar.h"
 
-Grammar::Grammar()
+Grammar::Grammar(string startChar) 
 {
-    nonTerminalVariables = {"S", "F", "D"};
-    terminalVariables = {"a", "b", "c"};
-    startingCharacter = newString = tempString = "S";
-
-    leftP = {"S", "F", "F", "S", "D", "D", "F"};
-    rightP = {"aF", "bF", "cD", "bS", "cS", "a", "a"};
-
+    startingCharacter = newString = tempString = startChar;
     flag = 0;
 }
-
+void Grammar::addProduction(string lhs, string rhs) 
+{
+    leftP.push_back(lhs);
+    rightP.push_back(rhs);
+    nonTerminalVariables.insert(lhs);
+    char temp = rhs.front();
+    string temp2; temp2 = temp;
+    if (temp2 != "X") terminalVariables.insert(temp2);
+}
 void Grammar::generateString()
 {
-    int nr = rand() % 6;
-
+    int nr = rand() % leftP.size();
     if (!flag) 
     {
         cout << startingCharacter;
         flag = 1;
     }
-    
     if (isupper(newString.back()) || tempString == startingCharacter)
     {
         string temp;
         temp.push_back(tempString.back());
         while (leftP.at(nr) != temp)
         {
-            nr = rand() % 6;
+            nr = rand() % leftP.size();
         }
-
         tempString = rightP.at(nr);
         newString.pop_back();
         newString.append(tempString);
         cout << " -> " << newString;
     }
-
-    if (isupper(newString.back()))
-        generateString();
+    if (isupper(newString.back())) generateString();
     else 
     {
         newString = tempString = startingCharacter;
@@ -47,57 +44,55 @@ void Grammar::generateString()
     }
 }
 
-vector<string> Grammar::toFiniteAutomaton()
+Automaton Grammar::toFiniteAutomaton()
 {
-    vector<string> v;
-    v.insert(v.end(), nonTerminalVariables.begin(), nonTerminalVariables.end());
-    v.insert(v.end(), terminalVariables.begin(), terminalVariables.end());
-    v.push_back(startingCharacter);
-    v.push_back("X");
-    v.insert(v.end(), leftP.begin(), leftP.end());
-    
-    vector<string> vt;
-    vector<string> vn;
-    for (auto &i : rightP)
+    Automaton FA(startingCharacter, {"X"});
+    for (int i = 0; i < leftP.size(); i++)
     {
-        string temp;
-        if (i.length() > 1)
-        {
-            temp = i.front();
-            vt.push_back(temp);
-            temp = i.back();
-            vn.push_back(temp);
-        }
-        else 
-        {
-            vt.push_back(i);
-            vn.push_back("X");
-        } 
+        string currSt = leftP.at(i);
+        string temp = rightP.at(i);
+        string label, nextSt;
+        label.append(1, temp.front());
+        if (temp.length() > 1) nextSt.append(1, temp.back());
+        else nextSt = "X";
+        FA.addTransitions(currSt, label, nextSt);
     }
-    v.insert(v.end(), vn.begin(), vn.end());
-    v.insert(v.end(), vt.begin(), vt.end());
-    return v;
+    return FA;
 }
-
 void Grammar::printGrammar()
 {
-    cout << endl;
-    cout << "Regular Grammar Rules" << endl;
+    cout << endl << "Grammar Rules" << endl;
     cout << "Vn = { ";
-    for (auto &i : nonTerminalVariables)
-    {
-        cout << i << ", ";
-    }
-    cout << "};" << endl;
-    cout << "Vt = { ";
-    for (auto &i : terminalVariables)
-    {
-        cout << i << ", ";
-    }
+    for (auto &i : nonTerminalVariables) {cout << i << ", ";}
+    cout << "};" << endl << "Vt = { ";
+    for (auto &i : terminalVariables) {cout << i << ", ";}
     cout << "};" << endl;
     cout << "Start character: " << startingCharacter << endl;
     cout << "Set of productions: P = " << endl;
     for (int i = 0; i < leftP.size(); i++)
         cout << leftP.at(i) << " -> " << rightP.at(i) << endl;
-    cout << endl;
+}
+void Grammar::chomskyType()
+{
+    int type;
+    int countLeft = 0, countRight = 0, countUpper = 0;
+    string left = leftP.at(0);
+    string right = rightP.at(0);
+    
+    for (int i = 0; i < leftP.size(); i++)
+    {
+        if (left.length() > 1) countLeft++;
+        if (right.length() > 2) countRight++;
+        for (int i = 0; i < left.length(); i++)
+            if (!isupper(left[i])) countUpper++;
+    }
+    if (!countLeft && !countRight) type = 3;
+    else if (!countLeft && countRight)  type = 2;
+    else if (countUpper == leftP.size()) type = 1;
+    else type = 0;
+
+    if (type == 3) cout << "This is a Regular Grammar" << endl; 
+    else if (type == 2) cout << "This is a Conntext-Free Grammar" << endl;
+    else if (type == 1) cout << "This is a Context-Sensitive Grammar" << endl;
+    else cout << "This is an Unrestricted Grammar" << endl;
 }
