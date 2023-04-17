431aeff83ffb2d5fb56675f83d6a3f4bc8b5cc65
zly-VON <113551094+zly-VON@users.noreply.github.com>
2023-04-17 12:56:30 +0300

    add CNF conversion

--- src/main.cpp
+++ src/main.cpp
@@ -7,44 +7,20 @@ using namespace std;
 
 int main(void)
 {
-    srand(time(0));    
-    
     Grammar g("S");
-    g.addProduction("S", "aF");
-    g.addProduction("F", "bF");
-    g.addProduction("F", "cD");
-    g.addProduction("S", "bS");
-    g.addProduction("D", "cS");
+    g.addProduction("S", "A");
+    g.addProduction("A", "aX");
+    g.addProduction("A", "bX");
+    g.addProduction("X", "e");
+    g.addProduction("X", "BX");
+    g.addProduction("X", "b");
+    g.addProduction("B", "AD");
+    g.addProduction("D", "aD");
     g.addProduction("D", "a");
-    g.addProduction("F", "a");
+    g.addProduction("C", "Ca");
     g.printGrammar();
-    g.chomskyType();
-    
-    Automaton nfa("q0", {"q2"});
-    nfa.addTransitions("q0", "b", "q0");
-    nfa.addTransitions("q0", "a", "q1");
-    nfa.addTransitions("q1", "c", "q1");
-    nfa.addTransitions("q1", "a", "q2");
-    nfa.addTransitions("q3", "a", "q1");
-    nfa.addTransitions("q3", "a", "q3");
-    nfa.addTransitions("q2", "a", "q3");
-    nfa.printFiniteAutomaton();
-    nfa.isDeterministic();
-    nfa.printGraph("nfa.dot");
 
-    Grammar g2 = nfa.toGrammar();
-    g2.printGrammar();
-
-    Automaton dfa = nfa.toDFA();
-    dfa.printFiniteAutomaton();
-    dfa.isDeterministic();
-    dfa.printGraph("dfa.dot");
-
-
-    string input = "int a = 5 + 2 * 3";
-    Lexer lexer(input);
-    std::vector<Token> tokens = lexer.tokenize();
-    lexer.printTokens(tokens);
+    g.toChomskyNormalForm();
 
     return 0;
 }
